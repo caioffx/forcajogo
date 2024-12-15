09ee07f41c7c93d33e417802d2c3bb276c20e9cc
caio f f <caiofkreitas@gmail.com>
2024-12-15 13:18:33 -0300

    Create a folder 'en version' and move the English files into it.

---
+++ en version/hang.h
@@ -0,0 +1,12 @@
+#define WORD_SIZE 20
+
+int letterExists();
+void opening();
+void makeGuess();
+void drawHangman();
+void selectWord();
+int hasWon();
+int isHanged();
+int alreadyGuessed(char letter);
+void insertWord();
+int wrongguesses();
